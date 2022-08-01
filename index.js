4fd256f6ed67491bca97a2b725bd87981d225558
Manh Quan <manhquan.05012002@gmail.com>
2022-08-01 18:07:30 +0700

    commit NIKA BE

---
+++ index.js
@@ -0,0 +1,21 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+const corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
+
+app.use(auth);
+app.use(router);
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {
+  console.log("Json server is Run!!!!!");
+});
